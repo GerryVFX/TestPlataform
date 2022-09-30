8ced0a13885d422fd7ffbded05065bdda620efdb
Gerardo <gerserrano@outlook.com>
2022-09-29 21:43:42 -0500

    Collisions

--- Plataformas/Assets/_Scripts/MovePlayer.cs
+++ Plataformas/Assets/_Scripts/MovePlayer.cs
@@ -8,8 +8,14 @@ public class MovePlayer : MonoBehaviour
     public float turnSpeed;
 
     public bool jump;
+    public bool isGrounded;
     public float jumpForce;
     Rigidbody _rigidbody;
+
+    public GameObject[] plataforms;
+
+    public Transform inPos;
+
     
     void Start()
     {
@@ -19,7 +25,7 @@ public class MovePlayer : MonoBehaviour
     
     void Update()
     {
-        if (jump)
+        if (jump && isGrounded) //isGrounded == false
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -37,6 +43,47 @@ public class MovePlayer : MonoBehaviour
 
         transform.Translate(0,0,moveVertical*speed*Time.deltaTime);
         transform.Rotate(0, moveHorizontal, 0 * turnSpeed * Time.deltaTime);      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUpJump"))
+        {
+            jump = true;
+            plataforms[0].GetComponent<Rigidbody>().useGravity = true;
+            plataforms[0].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            plataforms[1].GetComponent<Rigidbody>().useGravity = true;
+            plataforms[1].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            plataforms[2].GetComponent<Rigidbody>().useGravity = true;
+            plataforms[2].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        if (other.CompareTag("Danger"))
+        {
+            transform.position = inPos.position;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
 
